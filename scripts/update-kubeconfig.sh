734463ef13009bd78894142be5538697393ea321
TDW <mbwork1@yahoo.com>
2024-11-02 20:20:01 -0400

    updates3

--- scripts/update-kubeconfig.sh
+++ scripts/update-kubeconfig.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-response="$(aws eks list-clusters --region us-west-2 --output text | grep -i dominion-cluster 2>&1)" 
+response="$(aws eks list-clusters --region us-west-2 --output text | grep -i tdw-cluster 2>&1)" 
 if [[ $? -eq 0 ]]; then
-    echo "Success: Dominion-cluster exist"
-    aws eks --region us-west-2 update-kubeconfig --name dominion-cluster && export KUBE_CONFIG_PATH=~/.kube/config
+    echo "Success: TDW-cluster exist"
+    aws eks --region us-west-2 update-kubeconfig --name tdw-cluster && export KUBE_CONFIG_PATH=~/.kube/config
 
 else
-    echo "Error: Dominion-cluster does not exist"
+    echo "Error: TDW-cluster does not exist"
 fi
